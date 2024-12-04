b219e6d46b32a53d95e4cbc0e25fc71a54eeee09
KarlReiter16 <149010560+KarlReiter16@users.noreply.github.com>
2024-12-03 19:40:52 -0500

    Update Star.h
    Undo of left and right pointers (moving to BinaryTree.cpp)

--- Star.h
+++ Star.h
@@ -7,7 +7,6 @@ class Star {
     float mass; //0.5-8 solar masses 10^30
     pair<double, double> position; //0-24 right ascension, -90 - 90 declination
     int distance; //5-100 in lightyears
-    Star* left, * right;
 
     public:
     Star(string name, float mass, pair<double, double> position, int distance)
