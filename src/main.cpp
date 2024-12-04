d99d1c0f64a04856853494f230963095ff1a1d84
LrkRtr <karl.reiter16@gmail.com>
2024-12-04 16:18:06 -0500

    Fix main compliation errors
    Moved functions around to fix compile errors

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,193 @@
 #include "printStar.cpp"
 using namespace std;
 
+void invalidInput() {
+    cout << "Invalid input" << endl;
+}
+
+bool checkIntInput(string input) {
+    if(all_of(input.begin(), input.end(), ::isdigit)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool checkFloatInput(string input) {
+    if(all_of(input.begin(), input.end(), ::isdigit)) {
+        return true;
+    } else {
+        for(int i = 0; i < input.size(); i++) {
+            if(!isdigit(input[i]) && input[i] != '.') {
+                return false;
+            }
+        }
+        return true;
+
+    }
+}
+
+void preGenData(vector<Star> &stars, BinaryTree &tree) {
+    string input;
+    bool selected = false;
+    while(!selected) {
+        cout << "How many Stars would you like to generate (up to 1,000,000)" << endl;
+        cin >> input;
+        if(checkIntInput(input)) {
+            if(stoi(input) < 0 || stoi(input) > 1000000) {
+                invalidInput();
+            } else {
+                stars = generateData(stoi(input));
+                tree.InsertStarVector(tree.root, stars);
+                selected = true;
+            }
+        } else {
+            invalidInput();
+        }
+    }
+
+
+}
+
+void preInsertCustom(vector<Star> &stars, BinaryTree &tree) {
+    string input;
+    bool selected = false;
+    string newName;
+    float newMass;
+    double newAngle1;
+    double newAngle2;
+    int newDistance;
+
+
+
+
+    cout << "Name your star!" << endl;
+    cin >> input;
+    newName = input;
+    while(!selected) {
+        cout << "What is the mass of your star in solar masses! (0.50-8.00 2 decimal places)" << endl;
+        cin >> input;
+        if(checkFloatInput(input)) {
+            if(stof(input) < 0.5 || stof(input) > 8) {
+                invalidInput();
+            } else {
+                newMass = stof(input);
+                selected = true;
+            }
+        } else {
+            invalidInput();
+        }
+    }
+
+    selected = false;
+    while(!selected) {
+        cout << "What is the right ascension of your star! (0.0-24.0 up 1 decimal place)" << endl;
+        cin >> input;
+        if(checkFloatInput(input)) {
+            if(stod(input) < 0 || stod(input) > 24) {
+                invalidInput();
+            } else {
+                newAngle1 = stod(input);
+                selected = true;
+            }
+        } else {
+            invalidInput();
+        }
+    }
+
+    selected = false;
+    while(!selected) {
+        cout << "What is the declination of your star! (-90.0 to 90.0 1 decimal place)" << endl;
+        cin >> input;
+        if(checkFloatInput(input)) {
+            if(stod(input) < -90 || stod(input) > 90) {
+                invalidInput();
+            } else {
+                newAngle2 = stod(input);
+                selected = true;
+            }
+        } else {
+            invalidInput();
+        }
+    }
+
+    selected = false;
+    while(!selected) {
+        cout << "What is the distance of your star to Earth in lightyears! (5 to 100)" << endl;
+        cin >> input;
+        if(checkIntInput(input)) {
+            if(stoi(input) < 5 || stoi(input) > 100) {
+                invalidInput();
+            } else {
+                newDistance = stoi(input);
+                selected = true;
+            }
+        } else {
+            invalidInput();
+        }
+    }
+    pair<int, int> newPosition = make_pair(newAngle1, newAngle2);
+    Star newStar(newName, newMass, newPosition, newDistance);
+    Star *newStarPointer = &newStar;
+    tree.InsertStar(tree.root, newStarPointer);
+
+}
+
+Star preSearchBFS(BinaryTree tree) {
+    string input;
+    Node* resultNode;
+    Star* resultStar;
+    bool selected = false;
+    while (!selected) {
+        cout << "What star mass would you like to search for? (0.5 to 8 up to 2 decimal places)" << endl;
+        cin >> input;
+        if(checkFloatInput(input)) {
+            if(stof(input) < 0.5 || stof(input) > 8) {
+                invalidInput();
+            } else {
+                auto StartTime = chrono::high_resolution_clock::now();
+                resultNode = tree.BreadthFirstSearch(tree.root, stof(input));
+                auto EndTime = chrono::high_resolution_clock::now();
+                auto TimeTaken = std::chrono::duration_cast<std::chrono::milliseconds>(EndTime - StartTime); //This gives timetaken in milliseconds
+                cout << "Time taken to search: " << TimeTaken.count() << " miliseconds." << endl;
+
+                resultStar = resultNode->star;
+                resultStar->printStarInfo();
+                selected = true;
+            }
+        } else {
+            invalidInput();
+        }
+    }
+}
+
+void preSearchDFS(BinaryTree tree) {
+    string input;
+    Node* resultNode;
+    Star* resultStar;
+    bool selected = false;
+    while (!selected) {
+        cout << "What star mass would you like to search for? (0.5 to 8 up to 2 decimal places)" << endl;
+        cin >> input;
+        if(checkFloatInput(input)) {
+            if(stof(input) < 0.5 || stof(input) > 8) {
+                invalidInput();
+            } else {
+                auto StartTime = chrono::high_resolution_clock::now();
+                resultNode = tree.DepthFirstSearch(tree.root, stof(input));
+                auto EndTime = chrono::high_resolution_clock::now();
+                auto TimeTaken = std::chrono::duration_cast<std::chrono::milliseconds>(EndTime - StartTime); //This gives timetaken in milliseconds
+                cout << "Time taken to search: " << TimeTaken.count() << " miliseconds." << endl;
+
+                resultStar = resultNode->star;
+                resultStar->printStarInfo();
+                selected = true;
+            }
+        } else {
+            invalidInput();
+        }
+    }
+}
 
 int main() {
     
@@ -70,190 +257,3 @@ int main() {
     return 0;
 }
 
-void invalidInput() {
-    cout << "Invalid input" << endl;
-}
-
-void preGenData(vector<Star> &stars, BinaryTree &tree) {
-    string input;
-    bool selected = false;
-    while(!selected) {
-        cout << "How many Stars would you like to generate (up to 1,000,000)" << endl;
-        cin >> input;
-        if(checkIntInput(input)) {
-            if(stoi(input) < 0 || stoi(input) > 1000000) {
-                invalidInput();
-            } else {
-                stars = generateData(stoi(input));
-                tree.InsertStarVector(tree.root, stars);
-                selected = true;
-            }
-        } else {
-            invalidInput();
-        }
-    }
-    
-
-}
-
-void preInsertCustom(vector<Star> &stars, BinaryTree &tree) {
-    string input;
-    bool selected = false;
-    string newName;
-    float newMass;
-    double newAngle1;
-    double newAngle2;
-    int newDistance;
-
-    
-
-    
-        cout << "Name your star!" << endl;
-        cin >> input;
-        newName = input;
-        while(!selected) {
-            cout << "What is the mass of your star in solar masses! (0.50-8.00 2 decimal places)" << endl;
-            cin >> input;
-            if(checkFloatInput(input)) {
-                if(stof(input) < 0.5 || stof(input) > 8) {
-                    invalidInput();
-                } else {
-                    newMass = stof(input);
-                    selected = true;
-                }
-            } else {
-                invalidInput();
-            }
-        }
-
-        selected = false;
-        while(!selected) {
-            cout << "What is the right ascension of your star! (0.0-24.0 up 1 decimal place)" << endl;
-            cin >> input;
-            if(checkFloatInput(input)) {
-                if(stod(input) < 0 || stod(input) > 24) {
-                    invalidInput();
-                } else {
-                    newAngle1 = stod(input);
-                    selected = true;
-                }
-            } else {
-                invalidInput();
-            }
-        }
-
-        selected = false;
-        while(!selected) {
-            cout << "What is the declination of your star! (-90.0 to 90.0 1 decimal place)" << endl;
-            cin >> input;
-            if(checkFloatInput(input)) {
-                if(stod(input) < -90 || stod(input) > 90) {
-                    invalidInput();
-                } else {
-                    newAngle2 = stod(input);
-                    selected = true;
-                }
-            } else {
-                invalidInput();
-            }
-        }
-
-        selected = false;
-        while(!selected) {
-            cout << "What is the distance of your star to Earth in lightyears! (5 to 100)" << endl;
-            cin >> input;
-            if(checkIntInput(input)) {
-                if(stoi(input) < 5 || stoi(input) > 100) {
-                    invalidInput();
-                } else {
-                    newDistance = stoi(input);
-                    selected = true;
-                }
-            } else {
-                invalidInput();
-            }
-        }
-        pair<int, int> newPosition = make_pair(newAngle1, newAngle2);
-        Star newStar(newName, newMass, newPosition, newDistance);
-        Star *newStarPointer = &newStar;
-        tree.InsertStar(tree.root, newStarPointer);
-        
-}
-
-Star preSearchBFS(BinaryTree tree) {
-    string input;
-    Node* resultNode;
-    Star* resultStar;
-    bool selected = false;
-    while (!selected) {
-        cout << "What star mass would you like to search for? (0.5 to 8 up to 2 decimal places)" << endl;
-        cin >> input;
-        if(checkFloatInput(input)) {
-            if(stof(input) < 0.5 || stof(input) > 8) {
-                invalidInput();
-            } else {
-                auto StartTime = chrono::high_resolution_clock::now();
-                resultNode = tree.BreadthFirstSearch(tree.root, stof(input));
-                auto EndTime = chrono::high_resolution_clock::now();
-                auto TimeTaken = std::chrono::duration_cast<std::chrono::milliseconds>(EndTime - StartTime); //This gives timetaken in milliseconds
-                cout << "Time taken to search: " << timetaken.count() << " miliseconds." << endl;
-                
-                resultStar = resultNode->star;
-                resultStar->printStarInfo();
-                selected = true;
-            }
-        } else {
-            invalidInput();
-        }
-    }
-}
-
-void preSearchDFS(BinaryTree tree) {
-    string input;
-    Node* resultNode;
-    Star* resultStar;
-    bool selected = false;
-    while (!selected) {
-        cout << "What star mass would you like to search for? (0.5 to 8 up to 2 decimal places)" << endl;
-        cin >> input;
-        if(checkFloatInput(input)) {
-            if(stof(input) < 0.5 || stof(input) > 8) {
-                invalidInput();
-            } else {
-                auto StartTime = chrono::high_resolution_clock::now();
-                resultNode = tree.DepthFirstSearch(tree.root, stof(input));
-                auto EndTime = chrono::high_resolution_clock::now();
-                auto TimeTaken = std::chrono::duration_cast<std::chrono::milliseconds>(EndTime - StartTime); //This gives timetaken in milliseconds
-                cout << "Time taken to search: " << timetaken.count() << " miliseconds." << endl;
-                
-                resultStar = resultNode->star;
-                resultStar->printStarInfo();
-                selected = true;
-            }
-        } else {
-            invalidInput();
-        }
-    }
-}
-
-bool checkIntInput(string input) {
-    if(all_of(input.begin(), input.end(), ::isdigit)) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-bool checkFloatInput(string input) {
-    if(all_of(input.begin(), input.end(), ::isdigit)) {
-        return true;
-    } else {
-        for(int i = 0; i < input.size(); i++) {
-            if(!isdigit(input[i]) && input[i] != '.') {
-                return false;
-            }
-        }
-        return true;
-        
-    }
-}
