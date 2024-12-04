78664da05cdb3476680fefeed92e3166e8d3f8dd
LrkRtr <karl.reiter16@gmail.com>
2024-12-04 16:35:29 -0500

    fix GenerateData
    add return to generate data

--- src/GenerateData.cpp
+++ src/GenerateData.cpp
@@ -49,7 +49,7 @@ vector<Star> generateData(int numStars) {
         angle1 = (rand() % 241)/10; //0 to 24 1 decimal place
         angle2 = (rand() % 1800)/10 - 90; //-90 to 90 1 decimal place
         position = make_pair(angle1, angle2);
-
     }
+    return stars;
 
 }
