456dacee8457ca34cfa772229f04db1d7a969dcc
KarlReiter16 <149010560+KarlReiter16@users.noreply.github.com>
2024-12-03 19:42:56 -0500

    Update BinaryTree.cpp
    Makes BinaryTree as Nodes holding pointers to Star, Left and Right

---
+++ BinaryTree.cpp
@@ -0,0 +1,11 @@
+class BinaryTree {
+public:
+    Star* data;
+    BinaryTree* left, * right;
+
+    BinaryTree(Star* data) {
+        data = key;
+        left = nullptr;
+        right = nullptr;
+    }
+};
