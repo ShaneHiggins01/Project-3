4e76f6dd9b2a287aa766516ae5c2c924ecace0c6
ShaneHiggins01 <117116127+ShaneHiggins01@users.noreply.github.com>
2024-12-04 01:01:21 +0000

    generate data

--- main.cpp
+++ main.cpp
@@ -1,2 +1,14 @@
 #include "Star.h"
 #include "GenerateData"
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    vector<Star> stars;
+    stars = generateData(1);
+
+    cout << stars[0].getName();
+
+    return 0;
+}
